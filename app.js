20ad519a51d4dd4d84a9cee8f1cb3bc5e9172acb
joseph <=>
2022-07-05 00:25:59 +0300

    initial

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const express = require('express')
 const app = express()
-
+const request = require('request')
 const os = require('os')
+require('dotenv').config()
+const port = process.env.PORT || 80
+	app.get("/",(req,res)=>{
+	request('https://ifconfig.me/', function (error, response, body) {
+  		console.error('error:', error); // Print the error if one occurred
+  		console.log('statusCode:', response && response.statusCode); 
+  		console.log('body:', body); 
+  				res.send(`it works! client info is => ${os.arch()} ${os.platform()} ${body}  http headers => ${req.headers}`)
+		
+	})
 
-app.get("/",(req,res)=>{
-	res.send(`it works ${os.arch()} ${os.platform()}`)
-})
-
-app.listen(80,(()=>console.log('running')))
+});
+app.listen(port,(()=>console.log('running')))
